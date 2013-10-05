5dac801e744e2b7452c747590c2e57a186404abe
sudhir <sudorails@gmail.com>
2013-10-05 08:50:32 +0530

    secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VidhataCranes::Application.config.secret_token = '838eb77970c604f1deb971f84a95625e9c8d8d5020ce6509d2a38cfefdebb3c29c16c67337eb3831277f342c25e083d9190243771c51b60ba602ff287b72ebfe'
